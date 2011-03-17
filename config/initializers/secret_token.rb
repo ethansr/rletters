20e17a99d820b8b37f6a22408b6fd65034487217
root <root@evotext.(none)>
2011-03-17 11:22:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evotext::Application.config.secret_token = 'fc1e2e2d22b12122b8621421308cc5a6dab4bb6fb3c5825ad22d0dc49ed6ffa0b5f22dbf0d9de94b9a93e88b4bda39e0a27fded05c8134dd6568ec1559497a95'
