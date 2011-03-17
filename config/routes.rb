f92753978d92443280d568ded7715caffb4ea0a6
evoText Developers <evotext.server@gmail.com>
2011-03-17 16:48:48 -0400

    Functional 'show' method on a new 'doc' controller for single-document operations.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Evotext::Application.routes.draw do
   # See how all your routes lay out with "rake routes"
   root :to => "search#query"
 
+  match 'doc/:id' => 'doc#show'
+
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   match ':controller(/:action(/:id(.:format)))'
