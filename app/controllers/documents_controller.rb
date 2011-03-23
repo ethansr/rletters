b2a5825174e355d6ef0e5928a4ce9098c7692968
evoText Developers <evotext.server@gmail.com>
2011-03-23 14:42:05 -0400

    Add shiny GitHub-style button to the search page, theme it, and move it into a better location.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,9 @@
 class DocumentsController < ApplicationController
+  before_filter :default_attrs
+  def default_attrs
+    @no_searchbar = false
+  end
+  
   def index
     # FIXME: configurable per_page, sitewide
     page = params.has_key?(:page) ? Integer(params[:page]) : 1;
