c5b8cd06bbd00d24e7271e46697ae4dfb99adb6b
evoText Developers <evotext.server@gmail.com>
2011-05-03 18:52:12 -0400

    First step toward adding internal 'pages' for use as popup help dialogs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,24 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+  
+  def add_jquery_page(id, &block)
+    content_for :pages do
+      content_tag :div, :id => id, 'data-role' => 'page', 'data-theme' => 'd' do
+        block.call
+      end
+    end
+  end
+  
+  def add_help_page(id, &block)
+    add_jquery_page(id) do
+      content_tag :div, 'data-role' => 'header', 'data-theme' => 'd', 'data-position' => 'inline', 'data-backbtn' => 'false' do
+        content_tag :h1, "Help"
+      end
+      content_tag :div, 'data-role' => 'content' do
+        block.call
+        link_to request.request_uri, 'Close', 'data-role' => 'button', 'data-rel' => 'back', 'data-theme' => 'b'
+      end
+    end
+  end
 end
