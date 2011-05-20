26c67b43759b0a821606960ce0ca3aafa2248090
Charles Pence <charles@charlespence.net>
2011-05-20 12:19:50 -0400

    See if this variant makes the locale redirect work.

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -4,7 +4,9 @@ class OptionsController < ApplicationController
   def locale
     raise ActiveRecord::RecordNotFound if params[:locale].blank?
     raise ActiveRecord::RecordNotFound unless APP_CONFIG['available_locales'].include? params[:locale]
-    redirect_to send("root_#{params[:locale]}_path")
+    
+    I18n.locale = params[:locale]
+    redirect_to root_path
   end
   
   
