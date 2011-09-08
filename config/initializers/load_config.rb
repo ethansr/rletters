bd4a91d1458cda001ba5da72a74cc6441e249ce3
Charles Pence <charles@charlespence.net>
2011-09-08 19:21:37 -0400

    Add rpx_now, update some app_config parameters.

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,1 +1,5 @@
 APP_CONFIG = YAML.load_file(Rails.root.join('config', 'app_config.yml'))[Rails.env]
+
+# Load some configuration bits that live in APP_CONFIG
+RPXNow.api_key = APP_CONFIG['janrain_secret']
+
