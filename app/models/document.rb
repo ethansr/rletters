171af4b103756009417e3b624f0790fe9ae917d3
Charles Pence <charles@charlespence.net>
2011-09-22 22:19:44 -0400

    Documentation: fully document the Document class.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,56 +1,113 @@
+
+# Representation of a document in the Solr database.
+#
+# This class provides an ActiveRecord-like model object for documents hosted in
+# the RLetters Solr backend.  It abstracts both single-document retrieval and
+# document searching in class-level methods, and access to the data provided by
+# Solr in instance-level methods and attributes.
+#
+# @todo When working on serialization, make sure to integrate this with the sort
+#   of thing that +ActiveModel::Serializers+ does.
 class Document
-  # Make this class act like an ActiveRecord model, though it's
-  # not backed by the database (it's in Solr)
+  # Make this class act like an ActiveRecord model, though it's not backed by
+  # the database (it's in Solr)
   include ActiveModel::Validations
   include ActiveModel::Conversion
   include ActiveModel::Serialization
   extend ActiveModel::Naming
   extend ActiveModel::Translation
 
+  # Objects of this class are not persisted in the database
+  # @api private
+  # @return [Boolean] false
   def persisted?; false; end
+  # Objects of this class are always read-only
+  # @api private
+  # @return [Boolean] false
   def readonly?; true; end
 
+  # Throw an exception if +destroy+ is called on this object
+  # @api private
+  # @return nothing, always throws
   def before_destroy; raise ActiveRecord::ReadOnlyRecord; end
+  # Throw an exception if +Document.delete_all+ is called
+  # @api private
+  # @return nothing, always throws
   def self.delete_all; raise ActiveRecord::ReadOnlyRecord; end
+  # Throw an exception if +delete+ is called on this object
+  # @api private
+  # @return nothing, always throws
   def delete; raise ActiveRecord::ReadOnlyRecord; end
+
 
   # Bring in some helpers for parsing Solr's syntax
   extend SolrHelpers
 
-  # How to act appropriately ActiveModel-y:
-  # - To do serialization, integrate it into the ActiveModel::Serializers
-  # so it acts right.
-
-  # Return a single document (just bibliographic data) by SHA-1 checksum.
-  # The return value and options are documented in find_all_by_solr_query.
+  # Return a document (just bibliographic data) by SHA-1 checksum
+  #
+  # @api public
+  # @param [String] shasum SHA-1 checksum of the document to be retrieved
+  # @param [Hash] options see +find_all_by_solr_query+ for specification
+  # @return [Document] the document requested
+  # @raise [ActiveRecord::RecordNotFound] thrown if no matching document can
+  #   be found
+  # @see find_all_by_solr_query
+  # @example Look up the document with ID "1234567890abcdef1234"
+  #   doc = Document.find("1234567890abcdef1234")
   def self.find(shasum, options = {})
     set = find_all_by_solr_query({ :q => "shasum: #{shasum}", :qt => "precise" }, options)
     throw ActiveRecord::RecordNotFound if set.empty?
     set[0]
   end
 
-  # Return a single document (bibliographic data and full text) by
-  # SHA-1 checksum.  The return value and options are documented in 
-  # find_all_by_solr_query.
+  # Return a document (bibliographic data and full text) by SHA-1 checksum
+  #
+  # @api public
+  # @param [String] shasum SHA-1 checksum of the document to be retrieved
+  # @param [Hash] options see +find_all_by_solr_query+ for specification
+  # @return [Document] the document requested, including full text
+  # @raise [ActiveRecord::RecordNotFound] thrown if no matching document can
+  #   be found
+  # @see find_all_by_solr_query
+  # @example Get the full tet of the document with ID "1234567890abcdef1234"
+  #   text = Document.find_with_fulltext("1234567890abcdef1234").fulltext
   def self.find_with_fulltext(shasum, options = {})
     set = find_all_by_solr_query({ :q => "shasum: #{shasum}", :qt => "fulltext" }, options)
     throw ActiveRecord::RecordNotFound if set.empty?
     set[0]
   end
 
-  # Find a set of documents using a direct Solr query.  +params+ is an
-  # array that will be passed directly to Solr.  It can have (at least)
-  # the following keys:
+  # Find a set of documents using a direct Solr query
   #
-  #  - +params[:q]+ is a Solr query string "field:val field:val ..."
-  #  - +params[:qt]+ is the Solr query type.  The default query language
-  #    includes the types +standard+ (for stemmed, Google-like searching),
-  #    +precise+ (bibliographic data only), and +fulltext+ (bibliographic
-  #    data plus full document text)
+  # With the exception of processing the +:offset+ and +:limit+ options, 
+  # the +params+ array will be passed directly to Solr.
   #
-  # You can return a limited set of results either by setting +params[:start]+
-  # and +params[:rows]+, or by using the standard Rails options +:offset:+
-  # and +:limit+ as usually passed to <tt>ActiveRecord::find</tt>.
+  # @api public
+  # @param [Hash] params Solr query parameters  This is a hash that can have
+  #   (at least) the following keys:
+  #   - +params[:q]+: a Solr query string, typically of the format "field:val 
+  #     field:val ..."
+  #   - +params[:qt]+: the Solr query type.  In the default Solr configuration
+  #     provided with RLetters, valid values here are +standard+ (for stemmed,
+  #     Google-like searching), +precise+ (full Solr query syntax, returning
+  #     bibliographic data only), and +fulltext (full Solr query syntax,
+  #     returning both bibliographic data and full document text).
+  #   - +params[:start]+: alternate way to set +options[:offset]+.
+  #   - +params[:rows]+: alternate way to set +options[:limit]+.
+  # @param [Hash] options subset of the options usually passed to
+  #   +ActiveRecord::find+
+  # @option options [Integer] offset offset within the result set at which to
+  #   begin returning documents
+  # @option options [Integer] limit maximum number of results to return
+  # @return [Array] set of documents matching query.  An empty set will be
+  #   returned if no documents match.
+  #
+  # @example Return all documents in the collection (bad idea!)
+  #  collection = Document.find_all_by_solr_query({ :q => "*:*", :qt => "precise" })
+  # @example Return all documents which (fuzzily) match "general"
+  #  results = Document.find_all_by_solr_query({ :q => "general", :qt => "standard" })
+  # @example Return all documents published in 1983
+  #  results = Document.find_all_by_solr_query({ :q => "year:1983", :qt => "precise" })
   def self.find_all_by_solr_query(params, options = {})
     # Map from common Rails options to Solr options
     params[:start] = options[:offset] if options[:offset]
@@ -94,47 +151,97 @@ class Document
     documents.map { |attrs| Document.new(attrs) }
   end
 
-  # These are all the attributes that come directly out of the
-  # Solr schema
-  attr_reader :shasum, :doi, :authors, :title, :journal, :year,
-              :volume, :number, :pages, :fulltext
+  # @return [String] the SHA-1 checksum of this document
+  attr_reader :shasum
+  # @return [String] the DOI (Digital Object Identifier) of this document
+  attr_reader :doi
+  # @return [String] the document's authors, in a comma-delimited list
+  attr_reader :authors
+  # @return [String] the title of this document
+  attr_reader :title
+  # @return [String] the journal in which this document was published
+  attr_reader :journal
+  # @return [String] the year in which this document was published
+  attr_reader :year
+  # @return [String] the journal volume number in which this document was 
+  #   published
+  attr_reader :volume
+  # @return [String] the journal issue number in which this document was
+  #   published
+  attr_reader :number
+  # @return [String] the page numbers in the journal of this document, in the
+  #   format "start-end"
+  attr_reader :pages
+  # @return [String] the full text of this document.  May be +nil+ if the query
+  #   type used to retrieve the document does not provide the full text
+  attr_reader :fulltext
   
-  # Term vectors for this document.  This is provided in the following
-  # format:
+  # Term vectors for this document
   #
-  # doc.term_vectors["word"]
-  # # Term frequency (number of times this term appears in doc)
-  # doc.term_vectors["word"][:tf] = Integer
-  # # Term offsets
-  # doc.term_vectors["word"][:offsets] = Array
-  # # The start and end character offsets for "word" within
-  # # doc.fulltext
-  # doc.term_vectors["word"][:offsets][0] = Range
-  # ...
-  # # Term positions
-  # doc.term_vectors["word"][:positions] = Array
-  # # The word-index of this word in doc.fulltext
-  # doc.term_vectors["word"][:positions][0] = Integer
-  # ...
-  # # Number of documents in collection that contain "word"
-  # doc.term_vectors["word"][:df] = Float
-  # # Term frequency-inverse document frequency for "word"
-  # doc.term_vectors["word"][:tfidf] = Float
-  # doc.term_vectors["otherword"]
-  # ...
+  # The Solr server returns a list of information for each term in every 
+  # document.  The following data is provided (based on Solr server 
+  # configuration):
   #
+  # - +:tf+, term frequency: the number of times this term appears in
+  #   the given document
+  # - +:offsets+, term offsets: the start and end character offsets for
+  #   this word within +fulltext+.  Note that these offsets can be
+  #   complicated by string encoding issues, be careful when using them!
+  # - +:positions+, term positions: the position of this word (in
+  #   _number of words_) within +fulltext+.  Note that these positions
+  #   rely on the precise way in which Solr splits words, which is specified
+  #   by Unicode UAX 29.
+  # - +:df+, document frequency: the number of documents in the collection
+  #   that contain this word
+  # - +:tfidf+, term frequency-inverse document frequency: equal to (term
+  #   frequency / number of words in this document) * log(size of collection
+  #   / document frequency).  A measure of how "significant" or "important"
+  #   a given word is within a document, which gives high weight to words
+  #   that occur frequently in a given document but do _not_ occur in other
+  #   documents.
+  #
+  # @note This function may return +nil+, if the query type requested from
+  #   the Solr server does not return term vectors.
+  #
+  # @api public
+  # @return [Hash] term vector information.  The hash contains the following
+  #   keys:
+  #     term_vectors["word"]
+  #     term_vectors["word"][:tf] = Integer
+  #     term_vectors["word"][:offsets] = Array
+  #     term_vectors["word"][:offsets][0] = Range
+  #     # ...
+  #     term_vectors["word"][:positions] = Array
+  #     term_vectors["word"][:positions][0] = Integer
+  #     # ...
+  #     term_vectors["word"][:df] = Float
+  #     term_vectors["word"][:tfidf] = Float
+  #     term_vectors["otherword"]
+  #     # ...
+  # @example Get the frequency of the term "general" in this document
+  #   doc.term_vectors["general"][:tf]
   attr_reader :term_vectors
 
-  # Facets that were returned by the last search.  These are provided in
-  # the following format (year facets are by decade, from XXX0-XXX9):
+  # Content for facets defaults to +nil+.
+  @@facets = nil
+
+  # Faceted browsing information that was returned by the last search
   #
-  #   Document.facets = Hash
+  # For the purposes of faceted browsing, the Solr server (as configured by 
+  # default in RLetters) returns the number of items within the current search 
+  # with each author, journal, or publication decade.
+  #
+  # @api public
+  # @return [Hash] facets returned by the last search, +nil+ if none.
+  #   The hash contains the following keys:
   #     Document.facets[:author]["Particular Author"] = Integer
   #     Document.facets[:journal]["Particular Journal"] = Integer
   #     Document.facets[:year]["1980"] = Integer
+  #   The +:year+ facet is handled specially: the value for "1980" is the
+  #   facet count for the entire decade 1980-1989.
   #
-  # If the last search did not return any facet data, +facets+ will be +nil+.
-  @@facets = nil
+  # @example Get the number of documents in the last search published by W. Shatner
+  #   shatner_docs = Document.facets[:author]["W. Shatner"]
   def self.facets; @@facets; end
 
   # The shasum attribute is the only required one
@@ -147,5 +254,4 @@ class Document
       instance_variable_set("@#{name}".to_sym, value)
     end
   end
-
 end
