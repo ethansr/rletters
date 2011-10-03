ad998e5515003129b29a77290479d4de53ab8fd4
Charles Pence <charles@charlespence.net>
2011-10-03 17:36:00 -0400

    Finish functional tests on SearchController with tests on faceted browsing.

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -56,6 +56,65 @@ class SearchControllerTest < ActionController::TestCase
     assert_select 'div.leftcolumn ul li:nth-of-type(7)', "Botero, Carlos A., Andrew E. Mudge, Amanda M. Koltz, Wesley M. Hochachka, and Sandra L. Vehrencamp. 2008. “How Reliable are the Methods for Estimating Repertoire Size?”. Ethology 114: 1227-1238."
   end
 
+  test "should show login prompt if not logged in" do
+    stub_solr_response :standard_empty_search
+    session[:user] = nil
+    get :index
+    assert_select 'li[data-theme=e]', 'Log in to analyze results!'
+  end
+
+  test "should show create-dataset prompt if logged in" do
+    stub_solr_response :standard_empty_search
+    session[:user] = users(:john)
+    get :index
+    assert_select 'li', 'Create dataset from search'
+  end
+
+  test "should show author facets" do
+    stub_solr_response :precise_all_docs
+    get :index
+    assert_select 'div.rightcolumn ul:nth-of-type(3) li:nth-of-type(2)', 'Amanda M. Koltz1' do
+      assert_select "a[href=#{search_path(:fq => [ 'authors_facet:"Amanda M. Koltz"' ])}]"
+      assert_select 'span.ui-li-count', '1'
+    end
+  end
+
+  test "should show journal facets" do
+    stub_solr_response :precise_all_docs
+    get :index
+    # We show five author facet choices, then the journal facet, which is number 6.
+    assert_select 'div.rightcolumn ul:nth-of-type(3) li:nth-of-type(8)', 'Ethology10' do
+      assert_select "a[href=#{search_path(:fq => [ 'journal_facet:"Ethology"' ])}]"
+      assert_select 'span.ui-li-count', '10'
+    end
+  end
+
+  test "should show year facets" do
+    stub_solr_response :precise_all_docs
+    get :index
+    # We show five author facet choices, then the journal facet, then the year facets by count
+    assert_select 'div.rightcolumn ul:nth-of-type(3) li:nth-of-type(12)', '1990–19991' do
+      assert_select "a[href=#{search_path(:fq => [ 'year:[1990 TO 1999]' ])}]"
+      assert_select 'span.ui-li-count', '1'
+    end
+  end
+
+  test "should display remove all link with facets" do
+    stub_solr_response :precise_with_facet_koltz
+    get :index, { :fq => [ 'authors_facet:"Amanda M. Koltz"' ] }
+    assert_select 'div.rightcolumn ul:nth-of-type(3) li:nth-of-type(2)', 'Remove All' do
+      assert_select "a[href=#{search_path}]"
+    end
+  end
+
+  test "should display specific remove facet links" do
+    stub_solr_response :precise_facet_author_and_journal
+    get :index, { :fq => [ 'authors_facet:"Amanda M. Koltz"', 'journal_facet:"Ethology"' ] }
+    assert_select 'div.rightcolumn ul:nth-of-type(3) li:nth-of-type(3)', 'Authors: Amanda M. Koltz' do
+      assert_select "a[href=#{search_path(:fq => [ 'journal_facet:"Ethology"' ])}]"
+    end
+  end
+
   test "should correctly parse page, per_page in index" do
     default_sq = { :q => "*:*", :qt => "precise" }
     options = { :offset => 20, :limit => 20 }
