6873075805e7388f646f96b069d7d094eae87191
Charles Pence <charles@charlespence.net>
2011-11-22 22:37:33 -0500

    Whoops, the Capistrano recipe for static_assets was all wrong.

--- config/deploy/static_assets.rb
+++ config/deploy/static_assets.rb
@@ -1,7 +1,15 @@
 # -*- encoding : utf-8 -*-
 
-# Link the 'static_assets' folder into the public directory for each new
-# deployment
-task :after_update_code do
-  run "ln -s #{shared_path}/static_assets #{release_path}/public/static_assets"
+unless Capistrano::Configuration.respond_to?(:instance)
+  abort "This extension requires Capistrano 2"
+end
+
+Capistrano::Configuration.instance.load do
+
+  # Link the 'static_assets' folder into the public directory for each new
+  # deployment
+  task :after_update_code do
+    run "ln -s #{shared_path}/static_assets #{release_path}/public/static_assets"
+  end
+
 end
