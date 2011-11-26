53fed94b5a2f0beb86672b8e4e4941c6a3b60874
Charles Pence <charles@charlespence.net>
2011-11-26 17:56:14 -0500

    An attempt at a nice deployment strategy for the static text files.

---
+++ config/deploy/static_text.rb
@@ -0,0 +1,39 @@
+# -*- encoding : utf-8 -*-
+
+unless Capistrano::Configuration.respond_to?(:instance)
+  abort "This extension requires Capistrano 2"
+end
+
+def remote_file_exists?(full_path)
+  'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
+end
+
+Capistrano::Configuration.instance.load do
+
+  namespace :rletters do
+    
+    desc <<-DESC
+      Sync the static_text directory
+      
+      This task updates the Markdown files located in the static_text
+      directory (if new ones are available) and links them into the release
+      path.
+    DESC
+    task :copy_static_text do
+      run "mkdir -p #{shared_path}/static_text"
+      
+      Dir.glob('app/views/static/*.markdown.dist') do |file|
+        md_file = basename(file, '.dist')
+        
+        unless remote_file_exists? "#{shared_path}/static_text/#{md_file}"
+          run "cp #{release_path}/#{file} #{shared_path}/static_text/#{md_file}"
+        end
+      end
+      run "ln -sf #{shared_path}/static_text/* #{release_path}/app/views/static/"
+    end
+    
+    after "deploy:update_code", "rletters:copy_static_text"
+    
+  end
+  
+end
