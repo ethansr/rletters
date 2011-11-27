3954cee69bcce33962927d904cf62c4248147200
Charles Pence <charles@charlespence.net>
2011-11-26 22:28:45 -0500

    Update PATH for whenever, so that cron jobs will (hopefully) work.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,10 @@
 # -*- encoding : utf-8 -*-
 
-job_type :env_command, "cd :path && RAILS_ENV=:environment bundle exec :task :output"
+set :output, {:standard => nil}
+env :PATH, '/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin'
 
+
+job_type :env_command, "cd :path && RAILS_ENV=:environment bundle exec :task :output"
 
 every 1.hours do
   rake "db:sessions:expire"
