3e367f84fd9157d7cc49d4b3936f2dac491c62e2
Charles Pence <charles@charlespence.net>
2012-02-03 18:01:12 -0500

    Okay, enough of that.  Google Charts.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery_mobile
-
-// The JS files here have to be loaded in a certain order, so make sure
-// to use 'require' manually instead of 'require_tree'.
-//= require raphael
-//= require g.raphael
-//= require g.line
-
-//= require search
-//= require user
+//= require_tree .
 
 // Configure defaults for jQuery Mobile on all pages
 //$(document).bind("mobileinit", function(){
