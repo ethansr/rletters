c283fefd6f00f0330939ea0d8fcb51af77c629b7
Charles Pence <charles@charlespence.net>
2012-02-03 15:35:06 -0500

    Require the JS files for Raphael in order.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery_mobile
-//= require_tree .
+
+// The JS files here have to be loaded in a certain order, so make sure
+// to use 'require' manually instead of 'require_tree'.
+//= require raphael
+//= require g.raphael
+//= require g.line
+
+//= require search
+//= require user
 
 // Configure defaults for jQuery Mobile on all pages
 //$(document).bind("mobileinit", function(){
