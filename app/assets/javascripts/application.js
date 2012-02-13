87b7ed5860c5285d76b4f339bd9d74338496046f
Charles Pence <charles@charlespence.net>
2012-02-13 11:22:49 -0500

    Possible fix to AJAX page-loading bug.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@ $('div[data-role=page]').live('pageinit', function (event){
 
 // Load up the Google Visualization API
 google.load('visualization', '1.0', {'packages':['corechart']});
+
+// Call the given function only if it's defined, designed
+// to be used in window.setTimeout handlers when we may have
+// closed/removed from the DOM the page that contains the
+// function referenced in window.setTimeout.
+function callIfDefined(funcName) {
+  // If it's not defined, just do nothing
+  if (eval("typeof " + funcName) == 'function') {
+    eval(funcName + '()');
+  }
+}
