67a28b9ff46c014ed781cf5fa149ffe2ebfa22cf
Charles Pence <charles@charlespence.net>
2012-04-15 14:07:36 -0400

    Fix the whenever config.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 
 set :output, {:standard => nil}
-env :MAILTO, "#{YAML.load_file(File.expand_path('../app_config.yml', __FILE__))['all_environments']['ruby_path']}:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
+env :PATH, "#{YAML.load_file(File.expand_path('../app_config.yml', __FILE__))['all_environments']['ruby_path']}:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 env :MAILTO, "#{YAML.load_file(File.expand_path('../app_config.yml', __FILE__))['all_environments']['app_email']}"
 
 
