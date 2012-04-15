959ac5050cad6fef274db84ba1949298e53280fc
Charles Pence <charles@charlespence.net>
2012-04-15 13:50:11 -0400

    Finally fix tests on Ruby 1.8.
    On 1.8, Object#set_class_variable is a private method, so we have
    to call it using #send in the search helper spec.

--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -7,7 +7,7 @@ describe SearchHelper do
 
   describe '#num_results_string' do
     before(:each) do
-      Document.class_variable_set(:@@num_results, 100)
+      Document.send(:class_variable_set, :@@num_results, 100)
     end
 
     context 'when no search has been performed' do
@@ -34,7 +34,7 @@ describe SearchHelper do
   describe '#render_pagination' do
     context 'when we only have one page of results' do
       before(:each) do
-        Document.class_variable_set(:@@num_results, 1)
+        Document.send(:class_variable_set, :@@num_results, 1)
         @per_page = 10
         @page = 0
       end
@@ -47,7 +47,7 @@ describe SearchHelper do
     context 'when we have more than one page of results' do
       context 'when we are on the first page' do
         before(:each) do
-          Document.class_variable_set(:@@num_results, 100)
+          Document.send(:class_variable_set, :@@num_results, 100)
           @per_page = 10
           @page = 0
 
@@ -67,7 +67,7 @@ describe SearchHelper do
 
       context 'when we are in the middle' do
         before(:each) do
-          Document.class_variable_set(:@@num_results, 100)
+          Document.send(:class_variable_set, :@@num_results, 100)
           @per_page = 10
           @page = params[:page] = 5
 
@@ -87,7 +87,7 @@ describe SearchHelper do
 
       context 'when we are on the last page' do
         before(:each) do
-          Document.class_variable_set(:@@num_results, 100)
+          Document.send(:class_variable_set, :@@num_results, 100)
           @per_page = 10
           @page = params[:page] = 9
 
