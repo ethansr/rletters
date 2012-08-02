b93d09c911eaa56186e55cfa3f64e8b8c37a128b
Charles Pence <charles@charlespence.net>
2012-08-02 12:41:26 -0400

    Try delegating the event for the input independently of the page.

--- app/assets/javascripts/word_frequency.js
+++ app/assets/javascripts/word_frequency.js
@@ -1,8 +1,9 @@
 
 $('div[data-role=page]').live('pageshow', function (event, data) {
     $('#blocks_controls').toggle();
-    $('input[name=block_method_switch]').change(function() {
-        $('#count_controls').toggle();
-        $('#blocks_controls').toggle();
-        });
-    });
+});
+
+$('input[name=block_method_switch]').live('change', function(event, data) {
+    $('#count_controls').toggle();
+    $('#blocks_controls').toggle();
+});
